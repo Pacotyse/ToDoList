5d5ad5d506caa51b87379fc821d45e974bbf5367
Pacotyse <e.tison85@gmail.com>
2023-10-06 18:18:07 +0200

    ADD: Handling error removing task with no match

--- src/com/ToDoList/ToDoList.java
+++ src/com/ToDoList/ToDoList.java
@@ -24,11 +24,18 @@ class ToDoList implements TaskList {
 
     @Override
     public void removeTask(int taskId) {
+        // This method check if you found and match between the taskId provided and a task stored in the arraylist.
+        // If not, throw an error message and the user can try again.
+        boolean taskFound = false;
         for (int i = 0; i < tasks.size(); i++) {
             if (tasks.get(i).getId() == taskId) {
                 tasks.remove(i);
+                taskFound = true;
                 break;
             }
+        }
+        if (!taskFound) {
+            System.out.println("No task with the given ID exists.");
         }
     }
 }
